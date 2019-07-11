d9f3f8de1571afd6b05656e387f87a2cc9cc5cd2
Keith C. Ivey <keith@iveys.org>
2019-07-11 19:57:28 -0400

    break out normalizeDate() function in generate-gedcom.js

--- generate-gedcom.js
+++ generate-gedcom.js
@@ -267,14 +267,21 @@ function parseDatePlace(s) {
         throw new Error(`Unexpected date-place format "${s}"`);
     }
     const prefix = m[1] && (m[1] === 'roughly' ? 'EST' : 'ABT');
-    const place = m[3] && m[3].replace(/,? ([A-Z]{2})$/, ', $1');
-    // GEDCOM wants uppercase month, and requires 2 digits after slash in dual years
-    let date = m[2] && m[2].toUpperCase()
-        .replace(/(\d\d)\/\d$/, (m, m1) => m1 + '/' + (+m1 + 101).toString().substr(1, 2));
+    let date = normalizeDate(m[2]);
     if (date && prefix) {
         date = prefix + ' ' + date;
     }
+    const place = m[3] && m[3].replace(/,? ([A-Z]{2})$/, ', $1');
     return {date, place};
+}
+
+function normalizeDate(date) {
+    if (!date) {
+        return null;
+    }
+    // GEDCOM wants uppercase month, and requires 2 digits after slash in dual years
+    return date.toUpperCase()
+        .replace(/(\d\d)\/\d$/, (m, m1) => m1 + '/' + (+m1 + 101).toString().substr(1, 2));
 }
 
 function extractIds(s) {
